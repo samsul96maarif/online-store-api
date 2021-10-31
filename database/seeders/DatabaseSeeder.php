ee341d3ead9a3c260cc38411ff50462f84d055f9
sam <samsulma828@gmail.com>
2021-11-01 00:47:42 +0700

    register lod modul seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(AdminSeeder::class);
         $this->call(ProductSeeder::class);
+        $this->call(LogModulSeeder::class);
     }
 }
